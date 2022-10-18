df00acc37be7a19ec2b3c0c8728a65e76494572b
internet-goods <4577617+internet-goods@users.noreply.github.com>
2022-10-18 13:53:41 -0700

    Create firehol-lookup.sh

---
+++ firehol-lookup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+RUNDATE="$(date "+%Y-%m-%d")"
+echo $RUNDATE
+cd $RUNDATE
+echo "dest_ip,threat_list_name" > firehol-lookup.csv
+for netsetfilename in *.*set; do
+#make csv lookup file, remove internal ips since we use this for more than just egress filtering
+    echo Generating $netsetfilename.csv
+    cat $netsetfilename |awk -v env_var="$netsetfilename" '{print $0","env_var}' > $netsetfilename.csv
+    cat $netsetfilename.csv >> firehol-lookup.raw
+done
+
+echo "dest_ip,threat_list_name" > firehol-lookup.csv
+cat firehol-lookup.raw >> firehol-lookup.csv
